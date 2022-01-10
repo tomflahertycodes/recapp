dd0b7f48f6e4cf4055ccb8dc596ab4dcb5956f2a
thomas flaherty <thomasflaherty990@gmail.com>
2022-01-10 15:25:46 +0000

    edit and delete button added to jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,11 @@
 class JobsController < ApplicationController
+    before_action :find_job, only: [:show, :edit, :update, :destroy]
+
   def index
     @jobs = Job.all
   end
 
   def show
-    @job = Job.find(params[:id])
   end
 
   def new
@@ -17,7 +18,27 @@ class JobsController < ApplicationController
   redirect_to job_path(@job)
   end
 
+  def edit
+  end
+
+  def update
+    if @job.update(job_params)
+      redirect_to jobs_path(@job)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @job.destroy
+    redirect_to jobs_path(@job)
+  end
+
   private
+
+  def find_job
+    @job = Job.find(params[:id])
+  end
 
   def job_params
     params.require(:job).permit(:role, :location, :rate, :contract, :rate, :sector)
