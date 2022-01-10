63676f75e289371574e727ade7674b0b1842e0d1
thomas flaherty <thomasflaherty990@gmail.com>
2022-01-10 12:51:17 +0000

    create job feature added

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,14 +12,19 @@ class JobsController < ApplicationController
   end
 
   def create
-  @job = Job.new(params[:job])
-  @job.save
-  redirect_to job_path(@job)
+    @job = Job.new(job_params)
+    @job.user = current_user
+    @job.save
+    if @job.save
+      redirect_to jobs_path(@job)
+    else
+      render :new
+    end
   end
 
   private
 
   def job_params
-    params.require(:job).permit(:role, :location, :rate, :contract, :rate, :sector)
+    params.require(:job).permit(:role, :location, :rate, :contract, :sector)
   end
 end
